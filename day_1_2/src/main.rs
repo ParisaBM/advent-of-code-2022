67aac4a3a397462214bfd06645e2e009a647f6f2
Parisa Betel Miri <parisa.betel.miri@gmail.com>
2022-12-04 22:18:22 -0500

    day 1 and gitignore

---
+++ day_1_2/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::iter;
+use std::mem::swap;
+use std::path::Path;
+fn main() {
+    let file = File::open(Path::new("input")).unwrap();
+    let lines = io::BufReader::new(file).lines();
+    let mut highest_sums = [0; 3];
+    let mut current_sum = 0;
+    for line in lines {
+        let line = line.unwrap();
+        if line.is_empty() {
+            for i in 0..3 {
+                if current_sum > highest_sums[i] {
+                    swap(&mut current_sum, &mut highest_sums[i]);
+                }
+            }
+            current_sum = 0;
+        } else {
+            current_sum += line.parse::<i32>().unwrap();
+        }
+    }
+    for i in 0..3 {
+        if current_sum > highest_sums[i] {
+            swap(&mut current_sum, &mut highest_sums[i]);
+        }
+    }
+    println!("{}", highest_sums.iter().sum::<i32>());
+}
